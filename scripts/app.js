9d4b6e4c9eeb2729741fd59c7b222f0c8796298d
Luke Horvat <lukehorvat@gmail.com>
2015-03-12 03:50:58 +1100

    A bit more styling

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,18 @@ var sounds = [{
 }, {
   name: "Sound C",
   url: "//upload.wikimedia.org/wikipedia/commons/7/71/En-us-amidst_the_mists.ogg"
+}, {
+  name: "Sound D",
+  url: "//upload.wikimedia.org/wikipedia/commons/8/86/Dane_Zajc.ogg"
+}, {
+  name: "Sound E",
+  url: "//upload.wikimedia.org/wikipedia/commons/b/b4/Godzilla.ogg"
+}, {
+  name: "Sound F",
+  url: "//upload.wikimedia.org/wikipedia/commons/3/36/Sv-pappa.ogg"
+}, {
+  name: "Sound G",
+  url: "//upload.wikimedia.org/wikipedia/commons/f/fb/Fi-pyry.ogg"
 }];
 
 $(function() {
