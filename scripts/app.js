6130c301ac6817972f6b78134be07b436b4543ab
Luke Horvat <lukehorvat@gmail.com>
2015-03-12 02:13:05 +1100

    Hooked up sounds to buttons

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,34 @@
-soundManager.setup({
-  onready: function() {
-    var urls = [
-      "//upload.wikimedia.org/wikipedia/commons/d/d9/Wilhelm_Scream.ogg",
-      "//upload.wikimedia.org/wikipedia/commons/c/c8/Kk-kazakh.ogg",
-      "//upload.wikimedia.org/wikipedia/commons/7/71/En-us-amidst_the_mists.ogg"
-    ];
+var sounds = [{
+  name: "Sound A",
+  url: "//upload.wikimedia.org/wikipedia/commons/d/d9/Wilhelm_Scream.ogg"
+}, {
+  name: "Sound B",
+  url: "//upload.wikimedia.org/wikipedia/commons/c/c8/Kk-kazakh.ogg"
+}, {
+  name: "Sound C",
+  url: "//upload.wikimedia.org/wikipedia/commons/7/71/En-us-amidst_the_mists.ogg"
+}];
 
-    var sounds = urls.map(function(url) {
-      return soundManager.createSound({
-        url: url,
-        autoLoad: true
+$(function() {
+  soundManager.setup({
+    onready: function() {
+      sounds.forEach(function(sound) {
+        var button = $("<button />", {
+          text: sound.name,
+          disabled: "disabled"
+        }).on("click", function() {
+          soundManager.getSoundById(sound.name).play();
+        }).appendTo("#sounds");
+
+        soundManager.createSound({
+          id: sound.name,
+          url: sound.url,
+          autoLoad: true,
+          onload: function() {
+            button.removeAttr("disabled");
+          }
+        });
       });
-    });
-
-    sounds.forEach(function(sound) {
-      sound.play();
-    });
-  }
+    }
+  });
 });
