7393a12770933037025cb6b45f7cfb13bbd4b96e
Luke Horvat <lukehorvat@gmail.com>
2015-04-23 01:46:51 +1000

    A cute little pulse-fade effect

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -8,17 +8,25 @@ $(() => {
     onready: () => {
       sounds.forEach(sound => {
         let button = $("<button />", {
-          text: sound.name
+          text: sound.name,
+          disabled: "disabled"
         }).on("click", () => {
           soundManager.stopAll();
           soundManager.getSoundById(sound.name).play();
         }).appendTo("#sounds").hide();
 
+        let pulseFade = () => button.fadeToggle(400, () => button.queue().length > 0 && pulseFade());
+        let startPulseFade = () => button.queue().length <= 0 && pulseFade();
+        let stopPulseFade = () => button.stop(true, true).show();
+
         soundManager.createSound({
           id: sound.name,
           url: `sounds/${sound.file}`,
           autoLoad: true,
-          onload: () => button.fadeIn(3000)
+          onload: () => button.fadeIn(1000, () => button.removeAttr("disabled")),
+          onplay: startPulseFade,
+          onstop: stopPulseFade,
+          onfinish: stopPulseFade
         });
       });
     }
