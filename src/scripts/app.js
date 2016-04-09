90604b9a9feb2a3c3c97511f639f4000f6f072d2
Luke Horvat <lukehorvat@gmail.com>
2016-04-09 22:54:21 +1000

    Analytics

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -37,3 +37,11 @@ function loadSound(sound) {
   // simultaneously. So load the sounds at random intervals instead.
   setTimeout(() => soundManager.getSoundById(sound.name).load(), Math.random() * 10000);
 }
+
+// Google Analytics.
+(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");
+ga("create", "UA-24505142-5", "auto");
+ga("send", "pageview");
