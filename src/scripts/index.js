cb46d197a1ce0d7a8f239534cab18e9b12eb7ae0
Luke Horvat <lukehorvat@gmail.com>
2017-09-11 16:36:13 +0200

    Eliminate jQuery dependency

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,40 +1,38 @@
-import $ from "jquery";
-import {soundManager} from "soundmanager2";
+import { soundManager } from "soundmanager2";
 import sounds from "./sounds";
 
-$(() => {
+loadSounds();
+
+function loadSounds() {
   soundManager.setup({
     debugMode: false,
     onready: () => sounds.forEach(loadSound)
   });
-});
+}
 
 function loadSound(sound) {
-  let button = $(`
-    <button type="button">
-      <span><i class="fa fa-cog fa-spin"></i> Loading&hellip;</span>
-    </button>`
-  ).appendTo("#sounds");
-  let pulseFade = () => button.fadeToggle(400, () => button.queue().length > 0 && pulseFade());
-  let startPulseFade = () => button.queue().length <= 0 && pulseFade();
-  let stopPulseFade = () => button.stop(true, true).show();
+  let button = document.createElement("button");
+  button.type = "button";
+  button.innerHTML = `<span><i class="fa fa-cog fa-spin"></i> Loading&hellip;</span>`;
+  document.querySelector("#sounds").appendChild(button);
 
   soundManager.createSound({
     id: sound.name,
     url: `sounds/${sound.file}`,
     onload: () => {
-      button.on("click", () => {
+      button.querySelector("span").innerHTML = sound.name;
+      button.onclick = () => {
         soundManager.stopAll();
         soundManager.getSoundById(sound.name).play();
-      }).children("span").text(sound.name);
+      };
     },
-    onplay: startPulseFade,
-    onstop: stopPulseFade,
-    onfinish: stopPulseFade
+    onplay: () => button.classList.add("fade"),
+    onstop: () => button.classList.remove("fade"),
+    onfinish: () => button.classList.remove("fade")
   });
 
-  // For some reason, mobile browsers don't handle loading all sounds
-  // simultaneously. So load the sounds at random intervals instead.
+  // For some reason, mobile browsers don't handle loading all sounds simultaneously.
+  // So load the sounds at random intervals instead.
   setTimeout(() => soundManager.getSoundById(sound.name).load(), Math.random() * 10000);
 }
 
